60627d28a506b8304500c8a0f81367cc2cc965f1
Aman Khan <amankhanaezakmi@gmail.com>
2023-03-29 16:14:05 +0530

    third commit

---
+++ List_append.py
@@ -0,0 +1,6 @@
+#This python program taks size and elements of an list form user and append it in a list. 
+num=int(input("Enter the size of the list"))
+mylist=[]
+for i in range(1,num+1):
+    a=int(input("Enter Element :"))
+    mylist.append(a)          
