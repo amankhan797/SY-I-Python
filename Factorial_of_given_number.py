50b51bbf6723bbafe8642e6128b8b390b41e945e
Aman Khan <amankhanaezakmi@gmail.com>
2023-03-31 09:57:37 +0530

    Factorial program in python

--- Factorial_of_given_number.py
+++ Factorial_of_given_number.py
@@ -1,6 +1,6 @@
-#Python program for factorial of a given number.
-num=int(input("Enter a number"))
-fact=1
+#This python program taks size and elements of an list form user and append it in a list. 
+num=int(input("Enter the size of the list"))
+mylist=[]
 for i in range(1,num+1):
-    fact=fact*i
-print("factorial of given number is : ",fact)
+    a=int(input("Enter Element :"))
+    mylist.append(a)          
